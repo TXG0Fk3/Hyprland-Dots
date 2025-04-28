e6aedd0550326fcfff2d620e183c522b9c3e1b8a
TXG0Fk3 <leovertonx12@outlook.com>
2025-04-28 16:03:58 -0300

    feat(yay): implement check & installation

--- install.sh
+++ install.sh
@@ -4,3 +4,23 @@
 if [ "$EUID" -ne 0 ]; then
   exec sudo "$0" "$@"
 fi
+
+# Check if YAY is available and install it if not
+if ! command -v yay &> /dev/null; then
+  echo "yay not found. Installing..."
+
+  pacman -S --noconfirm --needed git base-devel
+
+  TMPDIR=$(mktemp -d)
+  git clone https://aur.archlinux.org/yay.git "$TMPDIR/yay"
+
+  cd "$TMPDIR/yay"
+  makepkg -si --noconfirm
+
+  cd /
+  rm -rf "$TMPDIR"
+
+  echo "yay installed successfully."
+else
+  echo "yay it's already installed."
+fi
